3c13f763b28d280565f4377be80887a7d9ead4d9
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-05-08 15:56:20 +0300

    Add recursive sum

--- Chapter_4/Quick_Sort/src/main/kotlin/Main.kt
+++ Chapter_4/Quick_Sort/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 fun main() {
     println(quickSort(array = arrayListOf(10, 5, 2, 3, 3)))
+    println(sum(list = arrayListOf(10, 5, 2, 3, 3, 14)))
 }
 
 fun quickSort(array: List<Int>):List<Int> {
@@ -10,4 +11,8 @@ fun quickSort(array: List<Int>):List<Int> {
         val greater = array.filter { it > pivot }
         quickSort(less) + pivot + quickSort(greater)
     }
+fun sum (list: List<Int>): Int {
+    if (list.isEmpty()) return 0
+    return list[0] + sum(list.drop(1))
+}
 }
