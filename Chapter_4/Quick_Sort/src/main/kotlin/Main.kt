ced4b6e124c645dd428199ca63cd1507f747d4a9
Anastasia <ms.anastasia.bogdanova@gmail.com>
2023-05-08 15:58:14 +0300

    Add recursive count

--- Chapter_4/Quick_Sort/src/main/kotlin/Main.kt
+++ Chapter_4/Quick_Sort/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
     println(quickSort(array = arrayListOf(10, 5, 2, 3, 3)))
     println(sum(list = arrayListOf(10, 5, 2, 3, 3, 14)))
+    println(count(list = arrayListOf(10, 5, 2, 3, 3, 14)))
 }
 
 fun quickSort(array: List<Int>):List<Int> {
@@ -11,8 +12,14 @@ fun quickSort(array: List<Int>):List<Int> {
         val greater = array.filter { it > pivot }
         quickSort(less) + pivot + quickSort(greater)
     }
+}
+
 fun sum (list: List<Int>): Int {
     if (list.isEmpty()) return 0
     return list[0] + sum(list.drop(1))
 }
+
+fun count(list: List<Int>): Int {
+    if (list.isEmpty()) return 0
+    return 1 + count(list.drop(1))
 }
